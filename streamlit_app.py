20f4984a179e76dc8c4c06704afb81852e1a9ee7
maxomm <max.warsinke@googlemail.com>
2022-11-09 00:27:33 +0100

    added gradient

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,54 @@
 from PIL import Image, ImageOps
-from numpy import asarray, hstack
+import numpy as np
 import streamlit as st
 
 st.title("Nothing Filter")
 
-uploaded_file = st.file_uploader(
-    "image", type="jpg", accept_multiple_files=False
-)
+uploaded_file = st.file_uploader("image", type="jpg", accept_multiple_files=False)
 image = (
     Image.open(uploaded_file) if uploaded_file is not None else Image.open("input.jpg")
 )
 
 fixed_image = ImageOps.exif_transpose(image)
-new_img = asarray(fixed_image)
+new_img = np.asarray(fixed_image)
 
 SLICES = st.slider("Slices", 1, 20, 8) + 1
-# OFFSET = st.slider("Offset", 0 + slice_width, width - slice_width, 0)
-FILTER_STR = st.slider("Strength", 0.0, 1.0, 0.6, 0.1)
+FILTER_STR = st.slider("Spread", 0.0, 1.0, 0.6, 0.1)
+FACTOR = st.slider("Gradient", 0.90, 1.00, 0.90, 0.01)
 
 height, width = fixed_image.height, fixed_image.width
 slice_width = width / SLICES
 start_index = (width / 2) - (slice_width / 2)
 
 
+def add_gradient(array_section):
+    array = create_gradient(int(slice_width), height, (255, 255, 255), (0, 0, 0))
+    test = array_section * FACTOR + array * (1 - FACTOR)
+    return test
+
+
+def create_gradient(width, height, start_list, stop_list):
+    result = np.zeros((height, width, len(start_list)), dtype=np.float64)
+    for i, (start, stop) in enumerate(zip(start_list, stop_list)):
+        result[:, :, i] = np.tile(np.linspace(start, stop, width), (height, 1))
+    return result
+
+
 for i in range(SLICES):
     cur_idx = start_index + (i - (SLICES - 1) / 2) * slice_width * (1 - FILTER_STR)
-    end_idx = int(cur_idx + slice_width)
     cur_idx = int(cur_idx)
+    end_idx = int(cur_idx + slice_width)
+
     new = new_img[0:height, cur_idx:end_idx]
-    combined = hstack((combined, new)) if i > 0 else new_img[0:height, cur_idx:end_idx]
+    new = add_gradient(new)
+    combined = (
+        np.hstack((combined, new))
+        if i > 0
+        else add_gradient(new_img[0:height, cur_idx:end_idx])
+    )
 
 
-im = Image.fromarray(combined)
+im = Image.fromarray(np.uint8(combined))
 
 col1, col2 = st.columns(2)
 with col1:
