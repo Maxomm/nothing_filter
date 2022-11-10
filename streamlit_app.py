4e1e17fe59c332e82ce259aeca24689bc0ee4565
maxomm <max.warsinke@googlemail.com>
2022-11-10 11:05:03 +0100

    cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,37 +2,10 @@ from PIL import Image, ImageOps
 import numpy as np
 import streamlit as st
 
-st.title("Nothing Filter")
-
-
-uploaded_file = st.file_uploader(label="", type=["jpg", "png"], accept_multiple_files=False, label_visibility="hidden"
-)
-image = (
-    Image.open(uploaded_file) if uploaded_file is not None else Image.open("input.jpg")
-)
-
-
-img_format = str(image.format).lower()
-
-fixed_image = ImageOps.exif_transpose(image)
-converted_image = fixed_image.convert("RGB")
-new_img = np.asarray(converted_image)
-
-col1, col2 = st.columns(2)
-with col1:
-    with st.expander("Change Parameters"):
-        SLICES = st.slider("Slices", 1, 20, 8) + 1
-        FILTER_STR = st.slider("Spread", 0.0, 1.0, 0.6, 0.1)
-        FACTOR = st.slider("Gradient", 0.80, 1.00, 0.90, 0.01)
-
-height, width = fixed_image.height, fixed_image.width
-slice_width = width / SLICES
-start_index = (width / 2) - (slice_width / 2)
-
 
 def add_gradient(array_section):
     array = create_gradient(int(slice_width), height, (255, 255, 255), (0, 0, 0))
-    test = array_section * FACTOR + array * (1 - FACTOR)
+    test = array_section * 0.9 + array * (1 - 0.9)
     return test
 
 
@@ -43,31 +16,59 @@ def create_gradient(width, height, start_list, stop_list):
     return result
 
 
-for i in range(SLICES):
-    cur_idx = start_index + (i - (SLICES - 1) / 2) * slice_width * (1 - FILTER_STR)
-    cur_idx = int(cur_idx)
-    end_idx = int(cur_idx + slice_width)
+def apply_filter(input_array):
+    for i in range(slice_amt):
+        cur_idx = start_index + (i - (slice_amt - 1) / 2) * slice_width * (
+            1 - filter_str
+        )
+        cur_idx = int(cur_idx)
+        end_idx = int(cur_idx + slice_width)
 
-    new = new_img[0:height, cur_idx:end_idx]
-    new = add_gradient(new)
-    combined = (
-        np.hstack((combined, new))
-        if i > 0
-        else add_gradient(new_img[0:height, cur_idx:end_idx])
+        new_data = input_array[0:height, cur_idx:end_idx]
+        new_data = add_gradient(new_data) if gradient_enabled else new_data
+        new_image = np.hstack((new_image, new_data)) if i > 0 else new_data
+    return new_image
+
+
+if __name__ == "__main__":
+    st.title("Nothing Filter")
+
+    up_file = st.file_uploader(
+        label="Image",
+        type=["jpg", "png"],
+        accept_multiple_files=False,
+        label_visibility="hidden",
     )
 
+    image = Image.open(up_file) if up_file is not None else Image.open("input.jpg")
 
-im = Image.fromarray(np.uint8(combined))
+    img_format = str(image.format).lower()
+    fixed_image = ImageOps.exif_transpose(image)
+    converted_image = fixed_image.convert("RGB")
+    img_array = np.asarray(converted_image)
 
-with col2:
-    st.image(im)
+    left_column, right_column = st.columns(2)
+    with left_column:
+        with st.expander("Change Parameters"):
+            slice_amt = st.slider("Slice Amount", 1, 10, 8) + 1
+            filter_str = st.slider("Filter Strength", 0.0, 1.0, 0.6, 0.1)
+            gradient_enabled = st.checkbox("Enable Gradient", value=True)
 
-with col1:
-    im.save("output." + img_format)
-    with open("output." + img_format, "rb") as img:
-        btn = st.download_button(
-            label="Download",
-            data=img,
-            file_name="nothing_filter." + img_format,
-            mime="image/png",
-        )
+    height, width = fixed_image.height, fixed_image.width
+    slice_width = width / slice_amt
+    start_index = (width / 2) - (slice_width / 2)
+
+    filtered_img = apply_filter(img_array)
+    image = Image.fromarray(np.uint8(filtered_img))
+
+    with right_column:
+        st.image(image)
+
+        image.save("output." + img_format)
+        with open("output." + img_format, "rb") as img:
+            btn = st.download_button(
+                label="Download",
+                data=img,
+                file_name="nothing_filter." + img_format,
+                mime="image/png",
+            )
